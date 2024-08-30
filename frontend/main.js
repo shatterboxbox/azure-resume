cdc1e351f44c0dad01ec6ebcf2846cde9bdb9127
shatterboxbox <65777404+shatterboxbox@users.noreply.github.com>
2024-08-30 13:41:09 -0700

    deployed storage for frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,49 +1,21 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  getVisitCount();
+    getVisitCount();
 });
 
-const functionApi = "http://localhost:7071/api/todoitems/Counter/Counter";
-
+const localfunctionApi = "http://localhost:7071/api/todoitems/Counter/Counter";
+const functionApi = "https://getresumecountercelleray.azurewebsites.net/api/todoitems/Counter/Counter?code=64YWdFazRDDMXV7jJssYpez2tJ13saDcxkDHREspOEniAzFuifw18Q%3D%3D"
 
 const getVisitCount = () => {
-  let count = "gello";
-  fetch(functionApi)
-    .then((response) => {
-        return response.json();
-    })
-    .then(response => {
-        count = response;
-        //response.Body.count;
-         document.getElementById('counter').innerText = count;
-     })
+    let count = "gello";
+    fetch(functionApi)
+        .then((response) => {
+            return response.json();
+        })
+        .then(response => {
+            count = response;
+            //response.Body.count;
+            document.getElementById('counter').innerText = count;
+        })
     return count;
 }
-    
-    
-    
-      
-//       document.getElementById("counter").innerText = count;
-//     })
-    
-//   return count;
-// }
 
-
-
-// const getVisitCount = () => {
-//     let count = 30;
-//     fetch(functionApi)
-//       .then((response) => {
-//         return response.json;
-//       })
-//       .then((response) => {
-//         console.log("Website called function API.");
-//         count = response.Body;
-//         document.getElementById("counter").innerText = count;
-//       })
-//       .catch(function (error) {
-//         console.log(error);
-//       });
-//     return count;
-//   }
-  
